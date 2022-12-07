4600ceaed5b87218bbf7bf6bccc385bdb8441ca0
Alex Cheah <alexkcheah@gmail.com>
2022-12-07 17:48:44 +0800

    Migrate files

---
+++ Scala-Chat-Program/src/main/scala/com/hep88/ChatServer.scala
@@ -0,0 +1,58 @@
+package com.hep88
+import akka.actor.typed.ActorRef
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.receptionist.{Receptionist, ServiceKey}
+import com.hep88.Upnp.AddPortMapping
+import scalafx.collections.ObservableHashSet
+
+case class User (name: String , ref:ActorRef[ChatClient.Command])
+
+object ChatServer {
+  // Protocol
+  sealed trait Command
+  case class JoinChat(name:String, ref:ActorRef[ChatClient.Command]) extends Command
+  case class Leave(name: String, from: ActorRef[ChatClient.Command]) extends Command
+
+  // Service Key
+  val ServerKey: ServiceKey[ChatServer.Command] = ServiceKey("Server")
+
+  //Server state
+  val members = new ObservableHashSet[User]()
+
+  members.onChange((newSet, c) => {
+    import com.hep88.ChatClient._
+    val newList = MemberList(newSet.toList)
+    for (user <- newSet) {
+      user.ref ! newList
+    }
+  })
+
+  def apply(): Behavior[ChatServer.Command] =
+    Behaviors.setup { context =>
+      val upnpRef = context.spawn(Upnp(), Upnp.name)
+      upnpRef ! AddPortMapping(20000)
+
+      context.system.receptionist ! Receptionist.Register(ServerKey, context.self)
+
+      Behaviors.receiveMessage { message =>
+        message match {
+          case JoinChat(name, ref) =>
+            import com.hep88.ChatClient._
+            ChatServer.members += User(name, ref)
+            ref ! Joined(ChatServer.members.toList)
+            Behaviors.same
+
+          case Leave(name, from) =>
+            members -= User(name, from)
+            Behaviors.same
+        }
+      }
+    }
+}
+
+object ServerApp extends App { // Create server skeleton
+  val greeterMain: ActorSystem[ChatServer.Command] = ActorSystem(ChatServer(), "HelloSystem")
+
+}
